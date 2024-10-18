70a9b91928573ae5c5641e6cb9a95cc2b611ef30
joaopmaximo <jp.maximoo@gmail.com>
2024-10-17 23:59:17 -0300

    first commit

---
+++ Conta.java
@@ -0,0 +1,38 @@
+public abstract class Conta implements IConta {
+    
+    private static final int AGENCIA_PADRAO = 0001;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta() {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public void sacar(double valor) {
+    }
+
+    @Override
+    public void depositar(double valor) {
+    }
+
+    @Override
+    public void transferir(Conta contaDestino, double valor) {
+    }
+}
